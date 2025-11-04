9214a22bf3ef72c11875dc0f19426630f04cab81
Niall Quinlan <niallquinlan@gmail.com>
2025-11-04 22:57:29 +0000

    first

---
+++ src/client/java/ninja/trek/mobility/MobilityEnchantmentsClient.java
@@ -0,0 +1,10 @@
+package ninja.trek.mobility;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobilityEnchantmentsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
